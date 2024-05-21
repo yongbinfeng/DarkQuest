3654665575a70624357180861934945a2bfffa61
Yongbin Feng <ybfeng94@gmail.com>
2024-05-20 23:19:14 -0500

    fix for undefined behavior and alignment file

--- e1039-analysis/SimHits/setup_mye1039.sh
+++ e1039-analysis/SimHits/setup_mye1039.sh
@@ -4,4 +4,3 @@ echo $MY_E1039
 echo $DIR_TOP
 source $MY_E1039
 export LD_LIBRARY_PATH=$DIR_TOP/install/lib/:$LD_LIBRARY_PATH
-export DIR_CMANTILL=/seaquest/users/cmantill/DarkQuest/e1039-analysis/SimHits
